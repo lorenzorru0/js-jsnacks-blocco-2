486aa0e2f8335e89af9a3ef83dec38a682a8ecd7
lorenzorru0 <lorenzoorru999@gmail.com>
2021-08-27 12:23:13 +0200

    Fix description

--- snack 2/script.js
+++ snack 2/script.js
@@ -1,9 +1,9 @@
 //Inserisci un numero, se è pari stampa il numero,
 //se è dispari stampa il numero successivo
 var nuovoNumero = 0;
-nuovoNumero = parseInt(prompt("Inserisci un numero (for): "));
+nuovoNumero = parseInt(prompt("Inserisci un numero: "));
 while (isNaN(nuovoNumero)) {
-    nuovoNumero = parseInt(prompt("Errore! Inserisci un numero valido (for): "));
+    nuovoNumero = parseInt(prompt("Errore! Inserisci un numero valido: "));
 }
 if (nuovoNumero % 2 == 0 ) {
     document.getElementById("numero").innerHTML = 'Il numero inserito è pari: ' + nuovoNumero;
