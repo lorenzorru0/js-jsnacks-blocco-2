6dc6e6e7354e85ee55c15327427a7b83de7ceda7
lorenzorru0 <lorenzoorru999@gmail.com>
2021-08-27 13:07:26 +0200

    Add with no repeat

--- snack 3/script.js
+++ snack 3/script.js
@@ -2,10 +2,31 @@
 //lista di cognomi, Gatsby vuole generare una falsa lista di 3 invitati.
 var nomi = ["Lorenzo", "Marco", "Samuele", "Luca", "GianMarco", "Massimiliano"]
 var cognomi = ["Orrù", "Piras", "Maccioni", "Melis", "Porceddu", "Madrigali"]
+var invitatiNomi = [];
+var invitatiCognomi = [];
 
 for (var i = 0; i < 3; i++) {
-    var indiceCasualeNome = Math.floor(Math.random() * (nomi.length - 1));
-    document.getElementById("invitati").innerHTML += '<br>' + '. ' + (i+1) + ' ' + nomi[indiceCasualeNome] + ' ';
-    var indiceCasualeCognome = Math.floor(Math.random() * (cognomi.length - 1));
-    document.getElementById("invitati").innerHTML += cognomi[indiceCasualeCognome];
+    if (i = 0) {
+        var indiceCasualeNome = Math.floor(Math.random() * (nomi.length - 1));
+        document.getElementById("invitati").innerHTML += '<br>' + '. ' + (i+1) + ' ' + nomi[indiceCasualeNome] + ' ';
+        invitatiNomi.push(nomi[indiceCasualeNome]);
+        var indiceCasualeCognome = Math.floor(Math.random() * (cognomi.length - 1));
+        document.getElementById("invitati").innerHTML += cognomi[indiceCasualeCognome];
+        invitatiCognomi.push(cognomi[indiceCasualeCognome]);
+    } else {
+        var indiceCasualeNome = Math.floor(Math.random() * (nomi.length - 1));
+        while (invitatiNomi[i - 1] != nomi[indiceCasualeNome]) {
+            indiceCasualeNome = Math.floor(Math.random() * (nomi.length - 1));
+        }
+        document.getElementById("invitati").innerHTML += '<br>' + '. ' + (i + 1) + ' ' + nomi[indiceCasualeNome] + ' ';
+        invitatiNomi.push(nomi[indiceCasualeNome]);
+
+        var indiceCasualeCognome = Math.floor(Math.random() * (cognomi.length - 1));
+        while (invitatiCognomi[i - 1] != cognomi[indiceCasualeCognome]) {
+            indiceCasualeCognome = Math.floor(Math.random() * (cognomi.length - 1));
+        }
+        document.getElementById("invitati").innerHTML += cognomi[indiceCasualeCognome];
+        invitatiCognomi.push(cognomi[indiceCasualeCognome]);
+    }
+    
 }
