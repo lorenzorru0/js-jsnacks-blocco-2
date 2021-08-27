dcec9e540b43678ca8e60c1ab6d03cd044330926
lorenzorru0 <lorenzoorru999@gmail.com>
2021-08-27 12:55:27 +0200

    Fix

--- snack 3/script.js
+++ snack 3/script.js
@@ -5,9 +5,7 @@ var cognomi = ["Orrù", "Piras", "Maccioni", "Melis", "Porceddu", "Madrigali"]
 
 for (var i = 0; i < 3; i++) {
     var indiceCasualeNome = Math.floor(Math.random() * (nomi.length - 1));
-    console.log(indiceCasualeNome);
     document.getElementById("invitati").innerHTML += '<br>' + '. ' + (i+1) + ' ' + nomi[indiceCasualeNome] + ' ';
     var indiceCasualeCognome = Math.floor(Math.random() * (cognomi.length - 1));
-    console.log(indiceCasualeCognome);
     document.getElementById("invitati").innerHTML += cognomi[indiceCasualeCognome];
 }
